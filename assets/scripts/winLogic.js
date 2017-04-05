c4b601c127eda0a5ddc1497ecb3306d914aa20f2
angmas <angela.mashlan@safeco.com>
2017-04-05 14:41:05 -0400

    Created winLogic.js for determining a win or tie.
    This file contains:
    - a multi dimensional array to identify the 8
    sets of coordinates that has the possibility of having a win.
    - a test array of values that represents the 3 x 3 grid for the game
      which is used for testing the function that strings together the
      elements in the array according to the coordinate sets
    - the function that strings together the elements in the array
      according to the coordinate sets.
    - the function that loops through the above function 8 times. This
      returns an array of 3 character element combinations.

---
+++ assets/scripts/winLogic.js
@@ -0,0 +1,41 @@
+'use strict'
+// The following is to aid in console testing the function
+// const tray = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+// const game = require('./assets/scripts/winLogic.js')
+
+// the following array holds all the possible winning lanes for the game
+const winLanes = [
+  // horizontal lanes
+  [[0, 0], [1, 0], [2, 0]],
+  [[0, 1], [1, 1], [2, 1]],
+  [[0, 2], [1, 2], [2, 2]],
+
+  // vertical lanes
+  [[0, 0], [0, 1], [0, 2]],
+  [[1, 0], [1, 1], [1, 2]],
+  [[2, 0], [2, 1], [2, 2]],
+
+  // diagonal lanes
+  [[0, 0], [1, 1], [2, 2]],
+  [[0, 2], [1, 1], [2, 0]]
+]
+
+// function to string together elements into a 3 character string
+const ticTacToe = (tray, coords) =>
+  coords.map(c => tray[c[0] * 3 + c[1]]).join('')
+
+// function to loop through winLanes to create an array of 3 character
+// strings to check for wins or ties.
+const getLanes = function (tray, array) {
+  const lanes = []
+  for (let i = 0; i < array.length; i++) {
+    lanes.push(ticTacToe(tray, array[i]))
+  }
+  return lanes
+}
+
+module.exports = {
+  winLanes,
+  ticTacToe,
+  getLanes
+}
