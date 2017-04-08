8159d2d3c10b4d9bd53964397b6f2be5f48aa2c0
angmas <angela.mashlan@safeco.com>
2017-04-08 07:17:09 -0400

    Created file to hold game statistics functions.
    This has placeholder functions for all the functions
    I want to add to the game.

---
+++ assets/scripts/game/gameStats.js
@@ -0,0 +1,27 @@
+'use strict'
+const showTtlGames = function () {
+}
+
+const showTtlGamesCompleted = function () {
+
+}
+
+const showTtlGamesAbandoned = function () {
+
+}
+
+const showTtlGamesWon = function () {
+
+}
+
+const showTtlGamesLost = function () {
+
+}
+
+module.exports = {
+  showTtlGames,
+  showTtlGamesCompleted,
+  showTtlGamesAbandoned,
+  showTtlGamesWon,
+  showTtlGamesLost
+}
