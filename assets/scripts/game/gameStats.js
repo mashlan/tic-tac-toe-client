896bf3687d397d6522eba25e449da95f5a7361d0
angmas <angela.mashlan@safeco.com>
2017-04-09 13:32:22 -0400

    Added feature to display total games finished.
    -updated index.html to add element to support the display line
    -updated gameStats.js with function that returns total games finished.
      Used the filter array function to get that total instead of coding
      an api call to get a filtered list from the server.

--- assets/scripts/game/gameStats.js
+++ assets/scripts/game/gameStats.js
@@ -3,8 +3,10 @@ const showTtlGames = function (games) {
 
 }
 
-const showTtlGamesCompleted = function () {
-
+const getTtlGamesFinished = function (games) {
+  const allGamesFinished = games.filter(e => e.over)
+  console.log('all games finished: ', allGamesFinished)
+  return allGamesFinished.length
 }
 
 const showTtlGamesAbandoned = function () {
@@ -21,11 +23,11 @@ const showTtlGamesLost = function () {
 
 const displayGameStats = function (games) {
   $('#all-games').text('Total Games Played: ' + games.length)
+  $('#total-games-finished').text('Total Games Finished: ' + getTtlGamesFinished(games))
 }
 
 module.exports = {
   showTtlGames,
-  showTtlGamesCompleted,
   showTtlGamesAbandoned,
   showTtlGamesWon,
   showTtlGamesLost,
